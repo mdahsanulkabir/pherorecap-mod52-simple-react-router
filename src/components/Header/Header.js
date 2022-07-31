b18b94ae95377f98f892e0f71112265d07a07c5e
ahsan <makabir98@yahoo.com>
2022-07-31 15:14:32 +0600

    custom link setting done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
         <div>
             <h1>Welcome to my fancy routing website with header.</h1>
             <nav>
-                <Link to="/">HOME</Link>
-                <Link to="/friends">FRIENDS</Link>
-                <Link to="/about">ABOUT</Link>
+                <CustomLink to="/">HOME</CustomLink>
+                <CustomLink to="/friends">FRIENDS</CustomLink>
+                <CustomLink to="/about">ABOUT</CustomLink>
             </nav>
         </div>
     );
